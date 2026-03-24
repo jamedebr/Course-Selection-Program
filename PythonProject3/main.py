f4d2a86cc92f8c0b81ba9df01a6dc7450b46f876
jamedebr <jamedebr@woodland.on.ca>
2026-03-24 10:02:26 -0400

    b

--- PythonProject3/main.py
+++ PythonProject3/main.py
@@ -1,19 +1,56 @@
 from kivy.app import App
-from kivy.graphics import Line
-from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
 
 
-class MyWidget(Widget):
-        def on_touch_down(self, touch):
-            touch.ud['line'] = Line(points=(touch.x, touch.y))
-            self.canvas.add(touch.ud['line'])
+class LoginScreen(Button, GridLayout):
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text='User Name'))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+        self.add_widget(Label(text='Password'))
+        self.password = TextInput(password=False, multiline=False)
+        self.add_widget(self.password)
+        self.text = 'hi'
 
-        def on_touch_move(self, touch):
-            touch.ud['line'].points += [touch.x, touch.y]
+        self.bind(on_press=self.switch)
+
+    def switch(self, item):
+        if self.password == 'ABCD' and self.username == 'cyan':
+            myapp.screen_manager.current = 'Second'
+
+class SecondPage(Button):
+    def __init__(self):
+        super().__init__()
+        self.text = 'hi there'
+        self.bind(on_press=self.switch)
+
+    def switch(self, item):
+        myapp.screen_manager.current = 'First'
 
 class MyApp(App):
     def build(self):
-        return MyWidget()
+        self.screen_manager = ScreenManager()
 
-if __name__ == '__main__':
-    MyApp().run()
+        self.LoginScreen = LoginScreen()
+        screen = Screen(name='First')
+        screen.add_widget(self.LoginScreen)
+        self.screen_manager.add_widget(screen)
+
+        self.secondpage = SecondPage()
+        screen = Screen(name='Second')
+        screen.add_widget(self.secondpage)
+        self.screen_manager.add_widget(screen)
+
+        return self.screen_manager
+
+myapp = MyApp()
+myapp.run()
+
+# if __name__ == '__main__':
+#     MyApp().run()
