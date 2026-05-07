e2ecba8da924c453d5a2fd3061d52bf6ace1f1cd
jamedebr <jamedebr@woodland.on.ca>
2026-05-07 11:47:51 -0400

    working framework before any gps

--- PythonProject3/main.py
+++ PythonProject3/main.py
@@ -1,18 +1,111 @@
 from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
-from kivy.core.window import Window
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty, StringProperty, NumericProperty
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.clock import Clock
+
+# with open('accounts.txt', 'r') as file:
+#     lines = file.readlines()
+#     lines.sort(key=lambda lines: int(lines.split()[-1]))
+#
+
+class Login(Screen):
+    email = ObjectProperty(None)
+    password = ObjectProperty(None)
+
+    def submitButton(self):
+        with open("accounts.txt", "r") as file:
+            accounts = file.read()
+            if self.email.text not in accounts and '@woodland.on.ca' in self.email.text:
+                with open("accounts.txt", "a") as file:
+                    file.write('\n'+self.email.text + " " + self.password.text + " " + "0")
+                self.manager.current = 'home'
+            else:
+                self.manager.current = 'login'
+
+    def toSignInButton(self):
+        self.manager.current = 'signin'
+
+class SignIn(Screen):
+    email = ObjectProperty(None)
+    password = ObjectProperty(None)
+
+    def signInButton(self):
+        with open('accounts.txt', 'r') as file:
+            content = file.read()
+            if self.email.text+' '+self.password.text in content:
+                self.manager.current = 'home'
+            else:
+                self.manager.current = 'signin'
+    def toLogin(self):
+        self.manager.current = 'login'
+
+
+class Home(Screen):
+    def on_enter(self):
+        print("on_enter has run")
+        grid = self.ids.leaderboard
+        grid.clear_widgets()
+
+        userstats = self.ids.userstats
+
+        with open('accounts.txt', 'r') as file:
+            lines = file.readlines()
+            lines.sort(key=lambda lines: int(lines.split()[-1]), reverse=True)
+            rankcounter = 0
+            for item in lines:
+                rankcounter += 1
+                user = item.split(' ')
+                numlaps = user[2]
+                if rankcounter < 6:
+
+                    rank = Label(text = f"{rankcounter}")
+                    grid.add_widget(rank)
+
+                    username = user[0]
+                    username = username.replace('@woodland.on.ca', '')
+                    username = Label(text = username)
+                    grid.add_widget(username)
+
+                    numlaps = Label(text=numlaps)
+                    grid.add_widget(numlaps)
+
+                Login = self.manager.get_screen('login')
+                email_value = Login.ids.email.text
+
+                if email_value == "":
+                    SignIn = self.manager.get_screen('signin')
+                    email_value = SignIn.ids.email.text
+
+                if email_value in user:
+                    userstats_rank = Label(text=f"{rankcounter}")
+                    userstats.add_widget(userstats_rank)
+
+                    userstats_username = user[0]
+                    userstats_username = userstats_username.replace('@woodland.on.ca', '')
+                    userstats_username = Label(text=userstats_username)
+                    userstats.add_widget(userstats_username)
+
+                    userstats_numlaps = Label(text=f"{user[2]}")
+                    userstats.add_widget(userstats_numlaps)
 
 
 
-class Calculator(BoxLayout):
 
-
-class Periodic(App):
+class MyApp(App):
     def build(self):
-        return app
+        sm = ScreenManager()
+        sm.add_widget(Login(name='login'))
+        sm.add_widget(SignIn(name='signin'))
+        sm.add_widget(Home(name='home'))
 
+        sm.current = 'login'
+        return sm
 
 if __name__ == '__main__':
-    Periodic().run()
+    MyApp().run()
+
--- PythonProject3/testing stuff.py
+++ PythonProject3/testing stuff.py
@@ -1,79 +1,110 @@
-from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
+# with open('accounts.txt', 'r') as file:
+#     lines = file.readlines()
+#     lines.sort(key=lambda lines: int(lines.split()[-1]), reverse=True)
+# print(lines)
 
-# Create both screens. Please note the root.manager.current: this is how
-# you can control the ScreenManager from kv. Each screen has by default a
-# property manager that gives you the instance of the ScreenManager used.
+user = ["abcd", "efgh"]
 
+if len(user) >= 3:
+    numlaps = user[2]
+else:
+    numlaps = "0"
+    user.append("0")
 
-Builder.load_string("""
-
-<HomeScreen>:
-    FloatLayout:
-        Button:
-            text: 'Settings'
-            size_hint: 0.2, 0.1
-            pos_hint: {"x": 0.25, "y": 0.0}
-            on_press: root.manager.current = 'settings'
-            
-        Button:
-            text: 'Quit'
-            size_hint: 0.2, 0.1
-            pos_hint: {"x": 0.55, "y": 0.0}
-            on_press: root.manager.current = 'quit'
-            
-
-
-<SettingsScreen>:
-    FloatLayout:
-        Button:
-            text: 'This button has the settings'
-            size_hint: 0.2, 0.1
-            pos_hint: {"x": 0.25, "y": 0.0}
-        
-        Button:
-            text: 'Back to home'
-            size_hint: 0.2, 0.1
-            pos_hint: {"x": 0.55, "y": 0.0}
-            on_press: root.manager.current = 'home'
-            
-<QuitScreen>:
-    FloatLayout:
-        Button:
-            text: 'go back to home'
-            size_hint: 0.2, 0.1
-            pos_hint: {"x": 0.25, "y": 0.0}
-            on_press: root.manager.current = 'home'
-        Button:
-            text: 'confirm quit'
-            size_hint: 0.2, 0.1
-            pos_hint: {"x": 0.55, "y": 0.0}
-            on_press: TestApp().quit()
-""")
-
-# Declare both screens
-class HomeScreen(Screen):
-    pass
-
-class SettingsScreen(Screen):
-    pass
-
-class QuitScreen(Screen):
-    pass
-
-class TestApp(App):
-
-    def build(self):
-        # Create the screen manager
-        sm = ScreenManager()
-        sm.add_widget(HomeScreen(name='home'))
-        sm.add_widget(SettingsScreen(name='settings'))
-        sm.add_widget(QuitScreen(name='quit'))
-
-        return sm
-
-if __name__ == '__main__':
-    TestApp().run()
+print(user)
+# <Home>:
+#     GridLayout:
+#         cols: 3
+#
+#         Label:
+#             text: ''
+#
+#         GridLayout:
+#             id: leaderboard
+#             cols: 3
+#             spacing: 10
+#             padding: 10
+#
+#
+#
+#         Label:
+#             text: ''
+# from kivy.app import App
+# from kivy.lang import Builder
+# from kivy.uix.screenmanager import ScreenManager, Screen
+# from kivy.uix.boxlayout import BoxLayout
+# from kivy.uix.button import Button
+#
+# # Create both screens. Please note the root.manager.current: this is how
+# # you can control the ScreenManager from kv. Each screen has by default a
+# # property manager that gives you the instance of the ScreenManager used.
+#
+#
+# Builder.load_string("""
+#
+# <HomeScreen>:
+#     FloatLayout:
+#         Button:
+#             text: 'Settings'
+#             size_hint: 0.2, 0.1
+#             pos_hint: {"x": 0.25, "y": 0.0}
+#             on_press: root.manager.current = 'settings'
+#
+#         Button:
+#             text: 'Quit'
+#             size_hint: 0.2, 0.1
+#             pos_hint: {"x": 0.55, "y": 0.0}
+#             on_press: root.manager.current = 'quit'
+#
+#
+#
+# <SettingsScreen>:
+#     FloatLayout:
+#         Button:
+#             text: 'This button has the settings'
+#             size_hint: 0.2, 0.1
+#             pos_hint: {"x": 0.25, "y": 0.0}
+#
+#         Button:
+#             text: 'Back to home'
+#             size_hint: 0.2, 0.1
+#             pos_hint: {"x": 0.55, "y": 0.0}
+#             on_press: root.manager.current = 'home'
+#
+# <QuitScreen>:
+#     FloatLayout:
+#         Button:
+#             text: 'go back to home'
+#             size_hint: 0.2, 0.1
+#             pos_hint: {"x": 0.25, "y": 0.0}
+#             on_press: root.manager.current = 'home'
+#         Button:
+#             text: 'confirm quit'
+#             size_hint: 0.2, 0.1
+#             pos_hint: {"x": 0.55, "y": 0.0}
+#             on_press: TestApp().quit()
+# """)
+#
+# # Declare both screens
+# class HomeScreen(Screen):
+#     pass
+#
+# class SettingsScreen(Screen):
+#     pass
+#
+# class QuitScreen(Screen):
+#     pass
+#
+# class TestApp(App):
+#
+#     def build(self):
+#         # Create the screen manager
+#         sm = ScreenManager()
+#         sm.add_widget(HomeScreen(name='home'))
+#         sm.add_widget(SettingsScreen(name='settings'))
+#         sm.add_widget(QuitScreen(name='quit'))
+#
+#         return sm
+#
+# if __name__ == '__main__':
+#     TestApp().run()
